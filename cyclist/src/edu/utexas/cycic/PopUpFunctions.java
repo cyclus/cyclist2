54814a488cafd0469099fbe9b20205b5ae70d3f5
Robert <flanny65@gmail.com>
2016-07-22 09:07:19 -0500

    added missing files

---
+++ cyclist/src/edu/utexas/cycic/PopUpFunctions.java
@@ -0,0 +1,13 @@
+package edu.utexas.cycic;
+
+import javafx.scene.control.Dialog;
+
+public class PopUpFunctions {
+	static Dialog buildPopUp(){
+		Dialog dg = new Dialog();
+		dg.setContentText("YESA!!S");
+		
+		
+		return dg;
+	}
+}
